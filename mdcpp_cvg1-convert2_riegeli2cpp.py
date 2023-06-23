2a8378561496eed962b733caae26dd5dfdbcf7b1
SemteulGaram <scgtdy7151@gmail.com>
2023-06-23 17:15:12 +0900

    Cleanup: mdcpp_cvg1-convert2_riegeli2cpp.py

--- mdcpp_cvg1-convert2_riegeli2cpp.py
+++ mdcpp_cvg1-convert2_riegeli2cpp.py
@@ -9,7 +9,6 @@ import os
 import sys
 
 import riegeli
-
 import contest_problem_pb2
 
 DIR='/'.join(os.path.realpath(__file__).split('/')[0:-2]) + '/data/'
@@ -25,43 +24,67 @@ def _all_problems(filenames):
 
 def make_cpp_files(filenames, fileType):
   #data/cppfiles_batch_[correct,incorrect]_[test,train,valid]/[problem_id].txt
-  if not os.path.exists(DIR + 'cppfiles_batch' + CODE_TYPE[0] + fileType):
-    os.makedirs(DIR + 'cppfiles_batch' + CODE_TYPE[0] + fileType)
+  p_cpp_correct_batch = os.path.join(DIR, 'cppfiles_batch' + CODE_TYPE[0] + fileType)
+  p_cpp_incorrect_batch = os.path.join(DIR, 'cppfiles_batch' + CODE_TYPE[1] + fileType)
+  p_desc = os.path.join(DIR, 'descriptions' + fileType)
+  p_sample = os.path.join(DIR, 'samples' + fileType)
+  p_generated_sample = os.path.join(DIR, 'generated_samples' + fileType)
+
+  if not os.path.exists(p_cpp_correct_batch):
+    os.makedirs(p_cpp_correct_batch)
   
-  if not os.path.exists(DIR + 'cppfiles_batch' + CODE_TYPE[1] + fileType):
-    os.makedirs(DIR + 'cppfiles_batch' + CODE_TYPE[1] + fileType)
+  if not os.path.exists(p_cpp_incorrect_batch):
+    os.makedirs(p_cpp_incorrect_batch)
 
-  if not os.path.exists(DIR + 'descriptions' + fileType):
-    os.makedirs(DIR + 'descriptions' + fileType)
+  if not os.path.exists(p_desc):
+    os.makedirs(p_desc)
 
-  if not os.path.exists(DIR + 'samples' + fileType):
-    os.makedirs(DIR + 'samples' + fileType)
+  if not os.path.exists(p_sample):
+    os.makedirs(p_sample)
 
-  if not os.path.exists(DIR + 'generated_samples' + fileType):
-    os.makedirs(DIR + 'generated_samples' + fileType)
+  if not os.path.exists(p_generated_sample):
+    os.makedirs(p_generated_sample)
 
   for program_id, problem in enumerate(_all_problems(filenames)):
     # batch export format: \n¶¶¶\n[id]\n¶¶\n[code]\n¶¶¶\n[id]\n¶¶\n[code] ...
     # Correct code batch export
-    with open(DIR + 'cppfiles_batch' + CODE_TYPE[0] + fileType + '/' + str(program_id) + '.txt', 'w') as corrFile:
+    is_empty = True
+    p_cpp_correct_batch_file = os.path.join(p_cpp_correct_batch, str(program_id) + '.txt')
+    with open(p_cpp_correct_batch_file, 'w') as corrFile:
       for correct_id, correct in enumerate(problem.solutions):
         if correct.language==2: # 2: C++
+          is_empty = False
           corrFile.write('\n¶¶¶\n' + str(correct_id) + '\n¶¶\n' + correct.solution)
+    if is_empty:
+      os.remove(p_cpp_correct_batch_file)
 
     # Incorrect code batch export
-    with open(DIR + 'cppfiles_batch' + CODE_TYPE[1] + fileType + '/' + str(program_id) + '.txt', 'w') as incorrFile:
+    is_empty = True
+    p_cpp_incorrect_batch_file = os.path.join(p_cpp_incorrect_batch, str(program_id) + '.txt')
+    with open(p_cpp_incorrect_batch_file, 'w') as incorrFile:
       for incorrect_id, incorrect in enumerate(problem.incorrect_solutions):
         if incorrect.language==2: # 2: C++
+          is_empty = False
           incorrFile.write('\n¶¶¶\n' + str(incorrect_id) + '\n¶¶\n' + incorrect.solution)
+    if is_empty:
+      os.remove(p_cpp_incorrect_batch_file)
 
-    with open(DIR + 'descriptions' + fileType + '/' + str(program_id) + '.txt', 'w') as descFile:
+    # Description export
+    p_desc_file = os.path.join(p_desc, str(program_id) + '.txt')
+    with open(p_desc_file, 'w') as descFile:
       descFile.write(problem.description)
 
-    with open(DIR + 'samples' + fileType + '/' + str(program_id) + '.txt', 'w') as sampleFile:
+    # Sample export
+    p_sample_file = os.path.join(p_sample, str(program_id) + '.txt')
+    with open(p_sample_file, 'w') as sampleFile:
       sampleFile.write(str(problem.public_tests))
 
-    with open(DIR + 'generated_samples' + fileType + '/' + str(program_id) + '.txt', 'w') as generatedSampleFile:
+    # Generated sample export
+    p_generated_sample_file = os.path.join(p_generated_sample, str(program_id) + '.txt')
+    with open(p_generated_sample_file, 'w') as generatedSampleFile:
       generatedSampleFile.write(str(problem.generated_tests))
+
+
 
 def main():
   for fileType in FILETYPE:
